c62f20b2f90f9c51a6fc7c8a668a63c50ef7aaae
liam34 <indemandcoding@gmail.com>
2022-02-15 10:18:51 -0700

    commit update from #19

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'MyPortfolio',
+    description: 'web design and development portfolio',
+    copyright: 'this website is Copyright ©2020 by MyPortfolio.com',
+    contact: 'squarevendo@gmail.com'
+  }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,24 @@
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 
 export default function NavBar() {
+  const data = useStaticQuery(graphql`
+   {
+    site { 
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+  `
+    )
+
+    const { title } = data.site.siteMetadata
+
   return (
     <nav>
-        <h1>My Portfolio Page</h1>
+        <h1>{ title }</h1>
         <div className='links'>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
